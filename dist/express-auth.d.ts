c16568dd25b4001e27fb11d1d19098389cc88518
Steve Montgomery <steve@chums.com>
2021-05-07 09:41:14 -0600

    Added object userAuth to Express.Request for better typescript validation

--- dist/express-auth.d.ts
+++ dist/express-auth.d.ts
@@ -1,6 +1,6 @@
 import { UserValidation } from "./types";
-declare module "express" {
+declare module "express-serve-static-core" {
     interface Request {
-        userAuth?: UserValidation;
+        userAuth: UserValidation;
     }
 }
--- lib/express-auth.ts
+++ lib/express-auth.ts
@@ -1,8 +1,7 @@
 import {UserValidation} from "./types";
 
-declare module "express" {
+declare module "express-serve-static-core" {
     export interface Request {
-        userAuth?: UserValidation
+        userAuth: UserValidation
     }
 }
-
