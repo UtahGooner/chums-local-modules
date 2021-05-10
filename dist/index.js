b1939e50984607ca43298c4019ba7ab681c7cb11
Steve Montgomery <steve@chums.com>
2021-05-10 13:38:38 -0600

    Exporting more methods from mailer.ts

--- dist/index.js
+++ dist/index.js
@@ -10,7 +10,7 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.buildWorkBook = exports.buildXLSXHeaders = exports.resultToExcelSheet = exports.sendEmail = exports.parseSQL = exports.getSageCompany = exports.getDBCompany = exports.loadValidation = exports.validateRole = exports.validateUser = exports.getConnection = exports.pool = exports.mysql2Pool = void 0;
+exports.buildWorkBook = exports.buildXLSXHeaders = exports.resultToExcelSheet = exports.sendOldSESEmail = exports.getTs36 = exports.getTs = exports.getLogoImageAttachment = exports.sendGmail = exports.sendEmail = exports.parseSQL = exports.getSageCompany = exports.getDBCompany = exports.loadValidation = exports.validateRole = exports.validateUser = exports.getConnection = exports.pool = exports.mysql2Pool = void 0;
 var mysql_1 = require("./mysql");
 Object.defineProperty(exports, "mysql2Pool", { enumerable: true, get: function () { return mysql_1.mysql2Pool; } });
 Object.defineProperty(exports, "pool", { enumerable: true, get: function () { return mysql_1.mysql2Pool; } });
@@ -25,6 +25,11 @@ Object.defineProperty(exports, "getSageCompany", { enumerable: true, get: functi
 Object.defineProperty(exports, "parseSQL", { enumerable: true, get: function () { return utils_1.parseSQL; } });
 var mailer_1 = require("./mailer");
 Object.defineProperty(exports, "sendEmail", { enumerable: true, get: function () { return mailer_1.sendEmail; } });
+Object.defineProperty(exports, "sendGmail", { enumerable: true, get: function () { return mailer_1.sendGmail; } });
+Object.defineProperty(exports, "getLogoImageAttachment", { enumerable: true, get: function () { return mailer_1.getLogoImageAttachment; } });
+Object.defineProperty(exports, "getTs", { enumerable: true, get: function () { return mailer_1.getTs; } });
+Object.defineProperty(exports, "getTs36", { enumerable: true, get: function () { return mailer_1.getTs36; } });
+Object.defineProperty(exports, "sendOldSESEmail", { enumerable: true, get: function () { return mailer_1.sendOldSESEmail; } });
 var toXLSX_1 = require("./toXLSX");
 Object.defineProperty(exports, "resultToExcelSheet", { enumerable: true, get: function () { return toXLSX_1.resultToExcelSheet; } });
 Object.defineProperty(exports, "buildXLSXHeaders", { enumerable: true, get: function () { return toXLSX_1.buildXLSXHeaders; } });
--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,6 @@
 export {mysql2Pool, mysql2Pool as pool, getConnection} from './mysql';
 export {validateUser, validateRole, loadValidation} from './validate-user';
 export {getDBCompany, getSageCompany, parseSQL} from './utils';
-export {sendEmail} from './mailer';
+export {sendEmail, sendGmail, getLogoImageAttachment, getTs, getTs36, sendMailProps, sendOldSESEmail} from './mailer';
 export {resultToExcelSheet, buildXLSXHeaders, buildWorkBook, WorkBookSheets, ColumnNames} from './toXLSX';
 export * from './types';
